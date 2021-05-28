ceb37071a54d6dd8145452fdd8fb34f9f678f883
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2021-05-28 11:36:57 +0200

    evo 2

--- Portfolio/index.php
+++ Portfolio/index.php
@@ -4,35 +4,43 @@
             <?php include_once("header.php");?>
         </center>
     </div>
-    <body class="content">
+    <body class="content body1">
         <center>
             <div class="row">
+                <div class="row espacehaut"></div>
                 <img class="imgpres" src="assets/img/presentation.jpg">
+                <div class="col-lg-3"></div>
+                
                 <div class="col-lg-7">
-                </div>
-                <div class="col-lg-4">
+                
                     <article class="presentationgen">
                         Bienvenue sur mon Portfolio, il a été mis en place dans le but de vous présenter, les compétences acquises lors de mon alternance 
-                        , l'entreprise qui m'a accueilli lors de ces deux années d'alternances ainsi que mon projet professionnel.
-                        <br>
+                        ,<br> l'entreprise qui m'a accueilli lors de ces deux années d'alternances ainsi que mon projet professionnel.<br>
                         Vous trouverez içi des situations étudiées en formation ainsi qu'en entreprise, mes productions, des exemples de missions que j'ai pu réaliser 
-                        durant mon BTS, mais également d'autres ressources utiles (CV, Veilles, Tableau de compétences etc...)
+                        durant mon BTS, mais également d'autres ressources utiles (CV, Veilles, Tableau de compétences etc...)<br>
                         Bonne visite !
                     </article>
                 </div>
+                <div class="col-lg-2"></div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-5"></div>
+                <div class="col-lg-2">
+                    <h1 class="titrepresent">Mon entreprise d'acceuil</h1>
+                </div>
+                <div class="col-lg-5"></div>
             </div>
             <div class="row titreent">
                 <div class="col-lg-2"></div>
                 <div class="col-lg-8">
-                    <h1 class="titrepresent">MON ENTREPRISE</h1>
-                    <br>
                     <br>
                     <img class="imgpp" src="assets/img/pplogo.png">
                     <article class="presentationent">
-                        L'entreprise qui m'a accueilli est la Préfecture de Police, au sein du commissariat du 4ème arrondissement.
-                        Le service ou j'effectue ma formation est le STI (Service des Technologies de l'Information), qui fait parti de la DSPAP
+                        L'entreprise qui m'a accueilli est la Préfecture de Police, au sein du commissariat du 4ème arrondissement.<br>
+                        Le service ou j'effectue ma formation est le STI (Service des Technologies de l'Information),<br> qui fait parti de la DSPAP
                         (Direction de Sécurité de Proximité de l'Agglomération Parisienne) elle même au sein de la SDSO 
-                        (Sous-Direction des Services Opérationnels). Le STI compte environ 100 personnes.
+                        (Sous-Direction des Services Opérationnels).<br> Le STI compte environ 100 personnes.
                         L'équipe que j'ai intégré se nomme ETD (Etude Technique et Développement), elle est composée de 5 développeurs. 
                         Elle a pour activité de développer et de maintenir des application web internes a la Police. Parfois a l'échelle nationale 
                         mais principalement en Île-de-france.
--- Portfolio/missionformation.php
+++ Portfolio/missionformation.php
@@ -4,8 +4,69 @@
             <?php include_once("header.php");?>
         </center>
     </div>
-    <body class="content">
+    <body class="content body2">
+        <div class="row espacehaut2"></div>
         <center>
+            <div class="row">
+                <h1 class="titrepage">Mes missions en formation</h1>
+                <hr>
+                <br>
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4 mission">
+                    <h3>Création d'une calculatrice en Windows form</h3>
+                </div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-4 mission">
+                    <h3>Création d'un morpion en en C#</h3>
+                </div>
+                <div class="col-lg-1"></div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4 mission">
+                    <h3>Création d'un site web dynamique PPE</h3>
+                </div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-4 mission">
+                    <h3>Création d'un pendu en Javascript</h3>
+                </div>
+                <div class="col-lg-1"></div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4 mission">
+                    <h3>Création d'un SCRUD</h3>
+                </div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-4 mission">
+                    <h3>Journalisation de logs</h3>
+                </div>
+                <div class="col-lg-1"></div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4 mission">
+                    <h3>Création d'un chat en PHP</h3>
+                </div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-4 mission">
+                    <h3>Creation et modification de fichier en C#</h3>
+                </div>
+                <div class="col-lg-1"></div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-4 mission">
+                    <h3>Création d'une application de gestion de salons en C#</h3>
+                </div>
+                <div class="col-lg-2"></div>
+                <div class="col-lg-5 mission"></div>
+            </div>
+            <br>
         </center>
     </body>
     <div>
