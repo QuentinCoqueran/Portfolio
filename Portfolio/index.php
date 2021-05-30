3d9e1656f83b83998da68b9c4e1308c34271306a
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2021-05-30 15:57:19 +0200

    cleanning homepage

--- Portfolio/index.php
+++ Portfolio/index.php
@@ -12,39 +12,42 @@
                 <div class="col-lg-2"></div>
                 <div class="col-lg-8">
                     <article class="presentationgen">
-                        Bienvenue sur mon Portfolio, il a été mis en place dans le but de vous présenter, les compétences acquises lors de mon alternance 
-                        ,<br> l'entreprise qui m'a accueilli lors de ces deux années d'alternances ainsi que mon projet professionnel.<br>
+                        Bienvenue sur mon Portfolio, il a été mis en place dans le but de vous présenter, les compétences que j'ai pu acquérir lors de mon alternance 
+                        ,<br>ainsi que l'entreprise qui m'a accueilli lors de ces deux années d'alternance.<br>
                         Vous trouverez içi des situations étudiées en formation ainsi qu'en entreprise, mes productions, des exemples de missions que j'ai pu réaliser 
                         durant mon BTS, mais également d'autres ressources utiles (CV, Veilles, Tableau de compétences etc...)<br>
-                        Bonne visite !
+                        Bonne visite à vous !
                     </article>
                 </div>
-                <div class="col-lg-2"></div>
+                <div class="col-lg-2">
+                </div>
             </div>
             <br>
             <div class="row">
                 <div class="col-lg-5"></div>
-                <div class="col-lg-2">
-                    <h1 class="titrepresent">Mon entreprise d'acceuil</h1>
+                <div class="col-lg-2 titrepresent">
+                    <h1 class="titreent">MON ENTREPRISE D'APPRENTISSAGE</h1>
                 </div>
                 <div class="col-lg-5"></div>
             </div>
-            <div class="row titreent">
-                <div class="col-lg-2"></div>
+            <div class="row titreent2">
+                <div class="col-lg-2">
+                </div>
                 <div class="col-lg-8">
                     <br>
                     <img class="imgpp" src="assets/img/pplogo.png">
                     <article class="presentationent">
                         L'entreprise qui m'a accueilli est la Préfecture de Police, au sein du commissariat du 4ème arrondissement.<br>
                         Le service ou j'effectue ma formation est le STI (Service des Technologies de l'Information),<br> qui fait parti de la DSPAP
-                        (Direction de Sécurité de Proximité de l'Agglomération Parisienne) elle même au sein de la SDSO 
+                        (Direction de Sécurité de Proximité de l'Agglomération Parisienne), elle même au sein de la SDSO 
                         (Sous-Direction des Services Opérationnels).<br> Le STI compte environ 100 personnes.
-                        L'équipe que j'ai intégré se nomme ETD (Etude Technique et Développement), elle est composée de 5 développeurs. 
-                        Elle a pour activité de développer et de maintenir des application web internes a la Police. Parfois a l'échelle nationale 
+                        L'équipe que j'ai intégré se nomme ETD (Etude Technique et Développement), elle est composée de 6 développeurs. 
+                        Elle a pour activité de développer et de maintenir des applications web internes à la Police. Parfois à l'échelle nationale 
                         mais principalement en Île-de-france.
                     </article>
                 </div>
-                <div class="col-lg-2"></div>
+                <div class="col-lg-2">
+                </div>
             </div>
             <br>
             <br>
@@ -63,16 +66,18 @@
                 <div class="camenbert circlechart col-lg-2" data-percentage="60">C#</div>
                 <br>
                 <br>
+                <div class="col-lg-2"><i class="icocomp fab fa-html5"></i>&nbsp&nbsp<i class="icocomp fab fa-css3-alt"></i></div>
                 <div class="col-lg-2"><i class="icocomp fab fa-php"></i></div>
-                <div class="col-lg-2"><i class="icocomp fab fa-bootstrap"></i></div>
-                <div class="col-lg-2"><i class="icocomp fab fa-html5"></i><i class="fa fa-css3-alt"></i></div>
                 <div class="col-lg-2"><i class="icocomp fab fa-symfony"></i></div>
                 <div class="col-lg-2"><i class="icocomp fab fa-git"></i><i class="fa fa-docker"></i></div>
+                <div class="col-lg-2"><i class="icocomp fab fa-bootstrap"></i></div>
                 <div class="col-lg-2"><i class="icocomp fa fa-hashtag"></i></div>
                 <br>
+                <br>
+                <br>
+                <hr>
             </div>
-            <br>
-            <br>
+            <div class="row comp2"></div>
             <br>
         </center>
     </body>
