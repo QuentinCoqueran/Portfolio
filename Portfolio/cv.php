2c7c6f9c012bab6a4e0ca003c50f787026b78354
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2021-05-30 16:14:23 +0200

    cv

---
+++ Portfolio/cv.php
@@ -0,0 +1,19 @@
+<html>
+    <div>
+        <center>
+            <?php include_once("header.php");?>
+        </center>
+    </div>
+    <body class="content body1">
+        <div class="row espacehaut2"></div>
+        <center>
+            <div style="padding: 10px; box-shadow: 0px 0px 4px 3px rgba(0,0,0,0.1); background-color: white; border-radius: 7px;" align="center">
+                <embed src="assets/docs/CV_QCoqueran.pdf" type="application/pdf" style="min-height:1000px" width="100%">
+            </div>
+        </center>
+    </body>
+    <div>
+        <center>
+            <?php include_once("footer.php");?>
+        </center>
+    </div>
