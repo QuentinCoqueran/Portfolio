90d02e8c800627110f145a1729e3e18c43904f22
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2021-05-30 16:25:36 +0200

    espacebas

--- Portfolio/cv.php
+++ Portfolio/cv.php
@@ -7,10 +7,15 @@
     <body class="content body1">
         <div class="row espacehaut2"></div>
         <center>
-            <div style="padding: 10px; box-shadow: 0px 0px 4px 3px rgba(0,0,0,0.1); background-color: white; border-radius: 7px;" align="center">
-                <embed src="assets/docs/CV_QCoqueran.pdf" type="application/pdf" style="min-height:1000px" width="100%">
+            <div class="row">
+                <div class="col-lg-2"></div>
+                    <div class="col-lg-8" style="padding: 10px; box-shadow: 0px 0px 4px 3px rgba(0,0,0,0.1); background-color: white; border-radius: 7px;" align="center">
+                        <embed src="assets/docs/CV_QCoqueran.pdf" type="application/pdf" style="min-height:1000px" width="100%">
+                    </div>
+                <div class="col-lg-2"></div>
             </div>
         </center>
+        <div class="row espacebas"></div>
     </body>
     <div>
         <center>
