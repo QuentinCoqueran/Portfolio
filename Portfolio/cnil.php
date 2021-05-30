7b6daafd22920b67286e87cc8f940a559f343102
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2021-05-30 18:04:05 +0200

    Update cnil.php

--- Portfolio/cnil.php
+++ Portfolio/cnil.php
@@ -8,7 +8,7 @@
         <div class="row espacehaut"></div>
         <center>
             <div class="row textveilletech">
-                    <h1 class="titreveilletech">LE WIFI 6</h1>
+                    <h1 class="titreveilletech">LA CNIL</h1>
                     <hr class="hrblanc">
             </div>
         </center>
