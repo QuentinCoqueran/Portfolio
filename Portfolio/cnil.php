ddd9c3c63b8691a1017a36131743f91d6cb5ccb4
QuentinCoqueran <71134923+QuentinCoqueran@users.noreply.github.com>
2021-06-02 10:25:17 +0200

    veilles

--- Portfolio/cnil.php
+++ Portfolio/cnil.php
@@ -10,7 +10,54 @@
             <div class="row textveilletech">
                     <h1 class="titreveilletech"><b>LA CNIL</b></h1>
                     <hr class="hrblanc">
+                    
             </div>
+                
+            <div class="row textveilletech textewifi2">
+                <div class="col-lg-1"></div>
+                <div class="col-lg-5">
+                    <img class="imgcnil" src="assets/img/cnil.png">
+                </div>
+                <div class="col-lg-5">
+                    <h4>Une mission d'information</h4> <br>
+                    <br>
+                    <article>
+                        La CNIL est investie d’une mission générale d’information des personnes des droits et répond aux demandes des particuliers et des professionnels. <br>
+                        Elle mène des actions de communication grand public que ce soit à travers la presse, son site Internet, sa présence sur les réseaux sociaux ou en mettant <br>
+                        à disposition des outils pédagogiques. Directement sollicitée par de nombreux organismes, sociétés ou institutions pour conduire des actions de formation <br>
+                        et de sensibilisation au RGPD (Règlement Général sur la Protection des Données), la CNIL participe aussi à des colloques, <br>
+                        des salons ou des conférences pour informer et en même temps s’informer. <br>
+                        Elle fédère un collectif de plus de 60 organismes qui mènent des actions en faveur de l’éducation au numérique. <br>
+                    </article>
+                </div>
+                <div class="col-lg-1"></div>
+            </div>
+            <br>
+            <br>
+            <div class="row textveilletech textecnil">
+                
+                <div class="col-lg-3"></div>
+                <div class="col-lg-6">
+                    <article>
+                        <h4>Une mission de protection</h4> 
+                        <br>
+                        <br>
+                        La CNIL veille à ce que les citoyens accèdent efficacement aux données contenues dans les traitements les concernant. <br>
+                        Toute personne peut s’adresser à la CNIL en cas de difficulté dans l’exercice de ses droits en lui adressant une plainte. <br>
+                        Ces plaintes concernent : <br>
+                        <br>
+                        &nbsp-&nbspla réputation en ligne (demandes de suppression de contenus sur internet),<br>
+                        &nbsp-&nbsple commerce (opposition à recevoir des courriels publicitaires),<br>
+                        &nbsp-&nbsples ressources humaines (vidéosurveillance, géolocalisation des véhicules),<br>
+                        &nbsp-&nbspla banque et le crédit (contestation de l’inscription dans l’un des fichiers de la banque de France). <br>
+                        &nbsp-&nbspLorsque plusieurs personnes souhaitent déposer une plainte envers un même organisme, une action de groupe peut être exercée &nbsp&nbsp&nbsp&nbsppar l’intermédiaire d’une association. <br>
+                    </article>
+                </div>
+                <div class="col-lg-3"></div>
+            </div>
+            <br>
+            <br>
+            <a href="http://www.pearltrees.com/coqueranquentin"><button class="btn btn-light">Lien vers mon Pearltrees</button></a>
         </center>
         <div class="row espacebas"></div>
     </body>
--- Portfolio/wifi6.php
+++ Portfolio/wifi6.php
@@ -34,7 +34,7 @@
             <br>
             <br>
 
-            <div class="row textveilletech">
+            <div class="row textveilletech textewifi2">
                 <h4>Le WiFi a des numéros de version maintenant</h4><br>
                 <br>
                 <article>
@@ -53,66 +53,81 @@
                     WiFi 3 : Corresponds à la norme 802.11g, lancée en 2003 <br>
                 </article>
             </div>
-            <div class="row textveilletech">
+            <br>
+            <br>
+            <div class="row textveilletech textewifi2">
                     <br>
                     <h4>Un réseau WiFi plus rapide </h4><br>
                     <br>
-                    Comme d’habitude, la nouvelle norme WiFi permet des transferts de données plus rapides. <br>
-                    Si vous utilisez un seul appareil avec votre routeur WiFi, les vitesses du WiFi 6 devraient être 40% supérieures aux vitesses de la norme précédente (WiFi 5 = 802.11ac). <br>
-                    Pour réussir à atteindre un débit aussi haut, la norme WiFi 6 permet d’encoder les données de manière plus efficace. En gros : plus de données sont encodées dans la même fréquence radio.<br> 
-                    Les processeurs qui encodent et décodent ces données deviennent de plus en plus puissants et permettent de supporter la charge de travail supplémentaire. <br>
-                    Cette nouvelle norme WiFi permet même d’augmenter le débit sur la bande 2.4 Ghz. <br>
-                    Tandis que l’industrie est passée aux fréquences 5Ghz pour éviter les interférences, le WiFi 2.4 Ghz est toujours le meilleur pour traverser les obstacles. <br>
-                    De plus, les interférences sur le réseau 2.4 Ghz sont moins importantes aujourd’hui étant donné la disparition des vieux téléphones sans fils qui utilisaient cette fréquence. <br>
-                    A lire aussi : WiFi : Definition, historique et tout ce qu’il y a à savoir ? <br>
-                    <br>
+                    <article>
+                        Comme d’habitude, la nouvelle norme WiFi permet des transferts de données plus rapides. <br>
+                        Si vous utilisez un seul appareil avec votre routeur WiFi, les vitesses du WiFi 6 devraient être 40% supérieures aux vitesses de la norme précédente (WiFi 5 = 802.11ac). <br>
+                        Pour réussir à atteindre un débit aussi haut, la norme WiFi 6 permet d’encoder les données de manière plus efficace. En gros : plus de données sont encodées dans la même fréquence radio.<br> 
+                        Les processeurs qui encodent et décodent ces données deviennent de plus en plus puissants et permettent de supporter la charge de travail supplémentaire. <br>
+                        Cette nouvelle norme WiFi permet même d’augmenter le débit sur la bande 2.4 Ghz. <br>
+                        Tandis que l’industrie est passée aux fréquences 5Ghz pour éviter les interférences, le WiFi 2.4 Ghz est toujours le meilleur pour traverser les obstacles. <br>
+                        De plus, les interférences sur le réseau 2.4 Ghz sont moins importantes aujourd’hui étant donné la disparition des vieux téléphones sans fils qui utilisaient cette fréquence. <br>
+                        A lire aussi : WiFi : Definition, historique et tout ce qu’il y a à savoir ? <br>
+                        <br>
+                    </article>
             </div>
-            <div class="row textveilletech">
+            <br>
+            <br>
+            <div class="row textveilletech textewifi2">
                     <h4>Plus de batterie </h4><br>
                     <br>
-                    Un nouveau système de « Target Wake Time » ou « Temps d’Eveil de la Cible » (TWT) est aussi mis en place et signifie que votre smartphone, tablette, <br>
-                    PC ou tout autre appareil connecté devrait avoir une meilleure autonomie de batterie. <br>
-                    Lorsque le routeur WiFi communique avec un appareil (comme votre smartphone) il peut lui dire exactement quand mettre sa radio WiFi en <br>
-                    veille et quand se réveiller pour recevoir la transmission suivante. <br>
-                    Cela permettra d’économiser la batterie, la radio WiFi devrait passer plus de temps en veille et donc permettre une durée de vie prolongée pour la batterie. <br>
-                    C’est aussi pratique pour les objets connectés récents ayant peu de batterie et qui doivent se connecter en WiFi. <br>
-                    <br>
+                    <article>
+                        Un nouveau système de « Target Wake Time » ou « Temps d’Eveil de la Cible » (TWT) est aussi mis en place et signifie que votre smartphone, tablette, <br>
+                        PC ou tout autre appareil connecté devrait avoir une meilleure autonomie de batterie. <br>
+                        Lorsque le routeur WiFi communique avec un appareil (comme votre smartphone) il peut lui dire exactement quand mettre sa radio WiFi en <br>
+                        veille et quand se réveiller pour recevoir la transmission suivante. <br>
+                        Cela permettra d’économiser la batterie, la radio WiFi devrait passer plus de temps en veille et donc permettre une durée de vie prolongée pour la batterie. <br>
+                        C’est aussi pratique pour les objets connectés récents ayant peu de batterie et qui doivent se connecter en WiFi. <br>
+                        <br>
+                    </article>
             </div>
-            <div class="row textveilletech">
+            <br>
+            <br>
+            <div class="row textveilletech textewifi2">
                     <h4>De meilleures performances dans les zones denses</h4> <br>
                     <br>
-                    La technologie WiFi a tendance à être ralentie lorsque vous êtes dans une zone dense avec beaucoup d’autres appareils connectés autours de vous. <br>
-                    Imaginez un aéroport, un centre commercial ou un grand bureau avec tout le monde connecté en WiFi. Il est fort à parier que la connexion sera lente à cause de la congestion ! <br>
-                    La nouvelle norme WiFi 6 ou WiFi 802.11ax, comprend beaucoup de nouvelles technologies pour limiter ce problème. <br>
-                    Intel annonce que le WiFi 6 devrait améliorer la vitesse de connexion de chaque utilisateur pour être « au moins quatre fois meilleure » dans les zones denses ou avec beaucoup d’appareils connectés.<br> 
-                    Cela ne devrait d’ailleurs pas seulement se limiter aux zones denses. Cette technologie pourrait bien, aussi, avoir un effet à la maison si vous avez de nombreux appareils connectés à votre WiFi<br>
-                    , ou si vous vivez dans un appartement avec de nombreuses autres connections aux alentours. Congestion et WiFi 6 Vous n’avez pas vraiment besoin de connaître les détails pour comprendre. <br>
-                    Un point d’accès Wi-Fi 6 avec un périphérique Wi-Fi 6 fonctionnera mieux. Mais voici ce qui se passe sous le capot : Le WiFi 6 peut désormais diviser un canal WiFi en un grand nombre de sous-canaux.<br> 
-                    Chacun de ces sous-canaux peut transporter des données destinées à un appareil différent. C’est possible grâce à la technologie appelée « Orthogonal Frequency Division Multiple Access » ou « OFDMA ». <br>
-                    Cela permet au point d’accès WiFi de communiquer à plusieurs appareils simultanément. La nouvelle norme sans pilotes a aussi amélioré le MIMO – Multiple-In, Multiple-Out. <br>
-                    Cela implique le routeur dispose de plusieurs antennes pour pouvoir communiquer avec plus d’appareils simultanément. <br>
-                    Avec le WiFi 5 le routeur pouvaient déjà communiquer avec plusieurs appareils en même temps, mais ces appareils ne pouvaient pas répondre en même temps. <br>
-                    Le WiFi 6 dispose d’une version améliorée du Mu-MIMO qui permet aux périphériques de répondre au point d’accés en même temps. <br>
-                    Les points d’accès WiFi proches les uns des autres peuvent générer des congestions sur le réseau s’ils émettent sur le même canal WiFi. <br>
-                    Dans ce cas, le routeur écoute et attends d’avoir un canal libre avant de répondre. Avec le WiFi 6, les point d’accès WiFi proches entre eux peuvent être configurés <br>
-                    pour avoir différentes couleurs « Basic service set » ou « Ensemble de Service de Base » (BSS). Cette « couleur » est simplement un nombre entre 0 et 7.<br>
-                    Lorsqu’un un appareil vérifie si un canal est libre mais capte un faible signal avec une « couleur différente »,<br>
-                    il peut alors ignorer ce signal et emmètre quand même sans attendre. Cela devrait améliorer les performances réseau globales et les performances dans les zones denses. <br>
-                    Cette technologie est aussi appelée « Spatial frequency re-use » ou « réutilisation des fréquences spatiales ». Ces deux améliorations sont les plus intéressantes concernant le WiFi 6,<br>
-                    mais la nouvelle norme prévois aussi de nombreuses petites améliorations. Par exemple le WiFi6, permettra aussi une meilleure utilisation de la technologie Beamforming. <br>
-                    Quand pourrez-vous profiter du WiFi 6 ? Certains routeurs disponibles aujourd’hui font déjà leurs promo en annonçant le support WiFi 6 ou 802.11AX, mais la norme WiFi 6 n’est pas encore terminée. <br>
-                    De plus il n’existe pas encore d’appareil client (smartphone, tablette, PC…) équipé du WiFi 6 aujourd’hui. <br>
-                    L’alliance WiFi prévois et espère que la norme devrait être prête et les premiers appareils devraient faire leurs apparition pendant l’année 2019. <br>
-                    En fait, vous ne devriez même pas vraiment vous en soucier – Dans le futur, les nouveaux routeurs, les nouveaux smartphones et les nouveaux PC seront équipés de cette norme WiFi de base. <br>
-                    Comme toujours avec les appareils WiFi, il est nécessaire que l’appareil emetteur ET l’appareil récepteur soient équipés de cette nouvelle norme pour bénéficier des performances promises par<br>
-                    l’Alliance WiFi. En gros, si vous voulez profiter du WiFi 6 sur votre smartphone, il vous faudra un point d’accès WiFi qui émet en WiFi 6 (routeur WiFi) <br>
-                    et un smartphone qui supporte le WiFi 6 également. Si vous connectez un appareil qui ne supporte que la norme WiFi 5 à votre nouveau routeur WiFi 6, cette connexion sera faite en mode WiFi 5. <br>
-                    Cependant votre routeur peut continuer à utiliser le WiFi 6 avec un autre appareil simultanément ! <br>
+                    <article>
+                        La technologie WiFi a tendance à être ralentie lorsque vous êtes dans une zone dense avec beaucoup d’autres appareils connectés autours de vous. <br>
+                        Imaginez un aéroport, un centre commercial ou un grand bureau avec tout le monde connecté en WiFi. Il est fort à parier que la connexion sera lente à cause de la congestion ! <br>
+                        La nouvelle norme WiFi 6 ou WiFi 802.11ax, comprend beaucoup de nouvelles technologies pour limiter ce problème. <br>
+                        Intel annonce que le WiFi 6 devrait améliorer la vitesse de connexion de chaque utilisateur pour être « au moins quatre fois meilleure » dans les zones denses ou avec beaucoup d’appareils connectés.<br> 
+                        Cela ne devrait d’ailleurs pas seulement se limiter aux zones denses. Cette technologie pourrait bien, aussi, avoir un effet à la maison si vous avez de nombreux appareils connectés à votre WiFi<br>
+                        , ou si vous vivez dans un appartement avec de nombreuses autres connections aux alentours. Congestion et WiFi 6 Vous n’avez pas vraiment besoin de connaître les détails pour comprendre. <br>
+                        Un point d’accès Wi-Fi 6 avec un périphérique Wi-Fi 6 fonctionnera mieux. Mais voici ce qui se passe sous le capot : Le WiFi 6 peut désormais diviser un canal WiFi en un grand nombre de sous-canaux.<br> 
+                        Chacun de ces sous-canaux peut transporter des données destinées à un appareil différent. C’est possible grâce à la technologie appelée « Orthogonal Frequency Division Multiple Access » ou « OFDMA ». <br>
+                        Cela permet au point d’accès WiFi de communiquer à plusieurs appareils simultanément. La nouvelle norme sans pilotes a aussi amélioré le MIMO – Multiple-In, Multiple-Out. <br>
+                        Cela implique le routeur dispose de plusieurs antennes pour pouvoir communiquer avec plus d’appareils simultanément. <br>
+                        Avec le WiFi 5 le routeur pouvaient déjà communiquer avec plusieurs appareils en même temps, mais ces appareils ne pouvaient pas répondre en même temps. <br>
+                        Le WiFi 6 dispose d’une version améliorée du Mu-MIMO qui permet aux périphériques de répondre au point d’accés en même temps. <br>
+                        Les points d’accès WiFi proches les uns des autres peuvent générer des congestions sur le réseau s’ils émettent sur le même canal WiFi. <br>
+                        Dans ce cas, le routeur écoute et attends d’avoir un canal libre avant de répondre. Avec le WiFi 6, les point d’accès WiFi proches entre eux peuvent être configurés <br>
+                        pour avoir différentes couleurs « Basic service set » ou « Ensemble de Service de Base » (BSS). Cette « couleur » est simplement un nombre entre 0 et 7.<br>
+                        Lorsqu’un un appareil vérifie si un canal est libre mais capte un faible signal avec une « couleur différente »,<br>
+                        il peut alors ignorer ce signal et emmètre quand même sans attendre. Cela devrait améliorer les performances réseau globales et les performances dans les zones denses. <br>
+                        Cette technologie est aussi appelée « Spatial frequency re-use » ou « réutilisation des fréquences spatiales ». Ces deux améliorations sont les plus intéressantes concernant le WiFi 6,<br>
+                        mais la nouvelle norme prévois aussi de nombreuses petites améliorations. Par exemple le WiFi6, permettra aussi une meilleure utilisation de la technologie Beamforming. <br>
+                        Quand pourrez-vous profiter du WiFi 6 ? Certains routeurs disponibles aujourd’hui font déjà leurs promo en annonçant le support WiFi 6 ou 802.11AX, mais la norme WiFi 6 n’est pas encore terminée. <br>
+                        De plus il n’existe pas encore d’appareil client (smartphone, tablette, PC…) équipé du WiFi 6 aujourd’hui. <br>
+                        L’alliance WiFi prévois et espère que la norme devrait être prête et les premiers appareils devraient faire leurs apparition pendant l’année 2019. <br>
+                        En fait, vous ne devriez même pas vraiment vous en soucier – Dans le futur, les nouveaux routeurs, les nouveaux smartphones et les nouveaux PC seront équipés de cette norme WiFi de base. <br>
+                        Comme toujours avec les appareils WiFi, il est nécessaire que l’appareil emetteur ET l’appareil récepteur soient équipés de cette nouvelle norme pour bénéficier des performances promises par<br>
+                        l’Alliance WiFi. En gros, si vous voulez profiter du WiFi 6 sur votre smartphone, il vous faudra un point d’accès WiFi qui émet en WiFi 6 (routeur WiFi) <br>
+                        et un smartphone qui supporte le WiFi 6 également. Si vous connectez un appareil qui ne supporte que la norme WiFi 5 à votre nouveau routeur WiFi 6, cette connexion sera faite en mode WiFi 5. <br>
+                        Cependant votre routeur peut continuer à utiliser le WiFi 6 avec un autre appareil simultanément ! <br>
+                    </article>
                     <br>
             </div>
-            <div class="row textveilletech">
-                    <h4>Un numéro de version pour le WiFi.</h4> <br>
-                    <br>
+            <br>
+            <br>
+            <div class="row textveilletech textewifi2">
+                <h4>Un numéro de version pour le WiFi.</h4> <br>
+                <br>
+                <article>
                     Je suis très satisfait de ce changement pour des numéros de version, l’ancienne dénomination grâce à des chiffres et des lettres à la signification pas toujours très claire <br>
                     était vraiment compliquée à comprendre pour les débutants. C’est un changement clair et facile à mettre en place qui aurait dû être fait il y a longtemps. <br>
                     Cela permettra aux non-initiés de mieux comprendre les normes WiFi. Après tout, de nombreuses personnes pourraient améliorer les performances de leurs réseau à la maison en changeant tout <br>
@@ -121,10 +136,14 @@
                     de versions et continuer à utiliser le sigle 802.11AX à la place. D’après moi les principaux constructeurs vont continuer d’utiliser l’appellation AX pour l’intégrer à leurs gamme <br>
                     de produits déjà nommées en fonction des normes WiFi plus anciennes (Par exemple je vois mal Asus Sortir un routeur « RT-WiFi6 » mais plutôt continuer sa gamme « RT-AC » par les « RT-AX »). <br>
                     La nouvelle norme étant beaucoup plus claire je pense cependant que d’ici quelques années tous les constructeurs l’utiliseront.<br>
-                    <br>
                 </article>
-                <a href="http://www.pearltrees.com/coqueranquentin"><button class="btn btn-light">Lien vers mon Pearltrees</button></a>
+                <br>
+                <br>
+                
             </div>
+            <br>
+            <br>
+            <a href="http://www.pearltrees.com/coqueranquentin"><button class="btn btn-light">Lien vers mon Pearltrees</button></a>
         </center>
         <div class="row espacebas"></div>
     </body>
